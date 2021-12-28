4f5c8899a4250a0dbe495642fecb69a9bcc1e18d
CodeChef KCE Chapter <96730781+CodeChef-KCE-Chapter@users.noreply.github.com>
2021-12-28 09:55:26 +0530

    Add files via upload
    KCE_0001

---
+++ Inception/Strong_PassWord.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+class Solution{
+	static Scanner fs  = new Scanner(System.in);
+	static PrintWriter out = new PrintWriter(System.out);
+
+	public static void main(String[] args) {
+	   char[] a = fs.next().toCharArray();
+       char[] b =fs.next().toCharArray();
+       
+       Arrays.sort(a);
+       Arrays.sort(b);
+       int n = a.length;
+       char ans = b[n];
+       
+       for(int i=0;i<n;i++)
+        if(a[i] != b[i]){
+            ans = b[i];
+            break;
+        }
+        out.println(ans);
+        out.flush();
+		
+	}
+}
